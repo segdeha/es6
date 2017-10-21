d586871c467f3d80ec9d5c125105563b7724a921
Andrew Hedges <andrew@hedges.name>
2017-10-20 22:36:13 -0700

    Rename file to match class name

--- assets/js/prev-next.js
+++ assets/js/prev-next.js
@@ -11,7 +11,7 @@ export class PrevNextNav {
         this.nav = nav
     }
 
-    _getLesson(dir) {
+    getLesson(dir) {
         let el = this.nav.querySelector('.selected')[`${dir}ElementSibling`]
 
         let lesson = el && el.querySelector('a').dataset.lesson || null
@@ -21,14 +21,21 @@ export class PrevNextNav {
     }
 
     render() {
-        let { lesson: prevLesson, text: prevText } = this._getLesson('previous')
-        let { lesson: nextLesson, text: nextText } = this._getLesson('next')
+        let { lesson: prevLesson, text: prevText } = this.getLesson('previous')
+        let { lesson: nextLesson, text: nextText } = this.getLesson('next')
 
         let prev = prevLesson && `<a data-lesson="${prevLesson}">← ${prevText}</a>` || ''
         let next = nextLesson && `<a data-lesson="${nextLesson}">${nextText} →</a>` || ''
 
         let prevNextNav = document.createElement('DIV')
-        prevNextNav.innerHTML = `<nav><ul><li>${prev}</li><li>${next}</li></ul></nav>`
+        prevNextNav.innerHTML = `
+            <nav>
+                <ul>
+                    <li>${prev}</li>
+                    <li>${next}</li>
+                </ul>
+            </nav>
+        `
 
         this.el.append(prevNextNav)
     }
