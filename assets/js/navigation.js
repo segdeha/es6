480c8684296828570e828b9f26344c5472bf5086
Andrew Hedges <andrew@hedges.name>
2017-10-21 11:11:46 -0700

    Fix bug in navigation that caused lesson nav not to highlight after user had selected the colophon

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,15 +4,17 @@
 export class Navigation {
     static update(lesson) {
         let baseElement = document.querySelector('#everything-that-is-not-a-lesson nav')
+
         // select the selected selection and unselect it
         let selected = baseElement.querySelector('li.selected')
         if (selected) {
             selected.classList.remove('selected')
-            // select the newly selected selection
-            let newlySelected = baseElement.querySelector(`[data-lesson="${lesson}"]`)
-            if (newlySelected) {
-                newlySelected.parentNode.classList.add('selected')
-            }
+        }
+
+        // select the newly selected selection
+        let newlySelected = baseElement.querySelector(`[data-lesson="${lesson}"]`)
+        if (newlySelected) {
+            newlySelected.parentNode.classList.add('selected')
         }
     }
 }
